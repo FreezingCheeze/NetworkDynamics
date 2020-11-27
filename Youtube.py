dfe83e1f8015db9fb6f2823ce667717c677e7361
naimeds10 <giesdb@gmail.com>
2020-11-27 16:15:50 +0100

    ja

--- Youtube.py
+++ Youtube.py
@@ -4,9 +4,10 @@ import random as rand
 import ast
 
 api_key = 'AIzaSyDGbY8PRfEKNJRoQci4Pjx5id-0I5lm5SA' # Gies' API key
+bas_key = 'AIzaSyCiv00E35N5wnqTNcp8CAXmkICSXraG0-w'
 video_id = 'Vh_vi8qN4Cs'
 
-service = build('youtube', 'v3', developerKey=api_key)
+service = build('youtube', 'v3', developerKey=bas_key)
 
 
 # Takes the video id of the video to get data from, as well as the dictionary to put the stats into
@@ -42,15 +43,9 @@ def generate_data(v_id):
         v_id = video['id']['videoId'] # get its video id
 
         get_video_data(v_id, res)
-        print("Finished i! Res: ", res)
+        print("F", res)
 
     return res
-
-
-#data = generate_data(video_id)
-# for item, value in data.items():
-#     print(item, value)
-
 
 def read_data_from_file():
     with open("YoutubeData.txt", encoding="utf8") as f:
@@ -60,9 +55,9 @@ def read_data_from_file():
 
     return dictionary
 
-def plot_data():
-    dictionary = read_data_from_file()
-    views = sorted([int(y) for (x, y) in dictionary.values()], reverse=True)
+def plot_data(data):
+
+    views = sorted([int(y) for (x, y) in data.values()], reverse=True)
 
     plt.plot(views)
 
@@ -71,13 +66,37 @@ def plot_data():
     plt.title("Views of Videos")
     plt.show()
 
+
 def plot_normal_distribution():
     dictionary = read_data_from_file()
     views = [int(y) for (x, y) in dictionary.values()]
-    min = min(views)
-    max = max(views)
-    intervals = [x for x in range(min, max, (max-min) / 10)]
-    print(intervals)
+    min_views = min(views)
+    max_views = max(views)
+    step = int((max_views - min_views) / 100)
+    intervals = [x for x in range(min_views, max_views, step)]
+
+    vals = dict()
+    for count in views:
+        for val in intervals:
+            if val <= count <= val + step:
+                if vals.get(val) is not None:
+                    vals[val] = vals[val] + 1
+                else:
+                    vals[val] = 1
+
+    for val in intervals:
+        if vals.get(val) is None:
+            vals[val] = 0
+
+    vals = sorted(vals.items())
+    print(vals)
+    plt.plot([x for (x, y) in vals], [y for (x, y) in vals] )
+
+    plt.xlabel('Views')
+    plt.ylabel('number of videos')
+    plt.title("Views of Videos")
+    plt.show()
 
 
-plot_normal_distribution()
+plot_data(read_data_from_file())
+#plot_data(generate_data(video_id))
